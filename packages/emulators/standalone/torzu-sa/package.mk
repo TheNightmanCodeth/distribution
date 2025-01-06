89f38fa70f9976d413721bb445001d2c47b63929
TheNightmanCodeth <joseph.diragi@icloud.com>
2025-01-06 21:29:26 +0000

    torzu: builds

--- packages/emulators/standalone/torzu-sa/package.mk
+++ packages/emulators/standalone/torzu-sa/package.mk
@@ -7,7 +7,7 @@ PKG_SITE="https://notabug.org/litucks/torzu"
 PKG_URL="${PKG_SITE}.git"
 PKG_LICENSE="GPLv3"
 PKG_TOOLCHAIN="cmake"
-PKG_DEPENDS_TARGET="toolchain linux libzip glibc systemd pulseaudio boost mesa xwayland glew-cmake libevdev curl ffmpeg libpng zlib SDL2 qt6 llvm"
+PKG_DEPENDS_TARGET="toolchain linux libzip glibc systemd pulseaudio nlohmann-json enet boost mesa xwayland glew-cmake libevdev curl ffmpeg libpng zlib SDL2 lz4 qt6 libfmt llvm libusb libva"
 PKG_DEPENDS_UNPACK="spirv-headers spirv-tools"
 PKG_LONGDESC="Switch Emulator"
 
@@ -38,17 +38,19 @@ post_unpack() {
 }
 
 pre_build_target() {
-  echo
-  #cd ${PKG_BUILD} && git submodule update --init --recursive
-  # cd ${PKG_BUILD}/externals/vcpkg && git fetch --unshallow
-  # sed -e '/add_subdirectory(externals)/d' -i ${PKG_BUILD}/CMakeLists.txt
+	sed -i '10i \#define ARCHITECTURE_arm64' ${PKG_BUILD}/src/core/arm/dynarmic/dynarmic_cp15.cpp
   sed -i '3i include(../dynarmic/CMakeModules/CreateDirectoryGroups.cmake)' ${PKG_BUILD}/externals/glad/CMakeLists.txt
+	sed -i '/add_definitions(-DBOOST_ASIO_DISABLE_CONCEPTS)/d' ${PKG_BUILD}/CMakeLists.txt
+	## Don't build nx_tzdb. Cross compile is fkd
+	#sed -i '/add_subdirectory(nx_tzdb)/d' ${PKG_BUILD}/externals/CMakeLists.txt
+	## Build tzdb2nx and place in ${PKG_BUILD}/build/bin/tzdb2nx
 }
 
 pre_configure_target() {
   PKG_CMAKE_OPTS_TARGET+=" -DYUZU_USE_BUNDLED_VCPKG=OFF \
 			   -DENABLE_QT6=ON \
   			   -DYUZU_TESTS=OFF \
+					 -DYUZU_DOWNLOAD_TIME_ZONE_DATA=ON \
 			   -DYUZU_USE_EXTERNAL_VULKAN_HEADERS=ON \
 			   -DYUZU_USE_BUNDLED_SDL2=OFF \
 			   -DYUZU_USE_EXTERNAL_VULKAN_UTILITY_LIBRARIES=ON \
@@ -63,13 +65,17 @@ pre_configure_target() {
                            -DUSE_LIBEVDEV=ON \
                            -DUSE_SYSTEM_FAUDIO=OFF \
                            -DUSE_SYSTEM_SDL=ON \
-			   -DARCHITECTURE=aarch64 \
-			   -DARCHITECTURE_aarch64=1"
+													 -DARCHITECTURE=arm64 \
+													 -DYUZU_USE_FASTER_LD=ON \
+													 -DARCHITECTURE_arm64=ON"
 }
+#			   -DARCHITECTURE=aarch64 \
+#			   -DARCHITECTURE_aarch64=1"
+#}
 
 configure_target() {
-  export ARCH=aarch64
-  export ARCHITECTURE=aarch64
+	export ARCHITECTURE=arm64
+	export ARCHITECTURE_arm64=1
   mkdir -p ${PKG_BUILD}/build
   cd ${PKG_BUILD}/build
   echo "Executing (target): cmake ${CMAKE_GENERATOR_NINJA} ${TARGET_CMAKE_OPTS} ${PKG_CMAKE_OPTS_TARGET} .." | tr -s " "
@@ -79,10 +85,15 @@ configure_target() {
   cmake ${CMAKE_GENERATOR_NINJA} ${TARGET_CMAKE_OPTS} ${PKG_CMAKE_OPTS_TARGET} ..
 }
 
+#post_patch() {
+  #rm ${PKG_BUILD}/externals/nx_tzdb/tzdb_to_nx/src/tzdb2nx/CMakeLists.txt
+	#cp ${PKG_DIR}/extra/tzdb2nx-fix ${PKG_BUILD}/externals/nx_tzdb/tzdb_to_nx/src/tzdb2nx/CMakeLists.txt
+#}
+
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/bin
   mkdir -p ${INSTALL}/usr/share/torzu-sa
-  cp -rf ${PKG_BUILD}/bin/* ${INSTALL}/usr/share/torzu-sa
+  cp -rf ${PKG_BUILD}/build/bin/* ${INSTALL}/usr/share/torzu-sa
 
   cp -rf ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin
   chmod 755 ${INSTALL}/usr/bin/*
